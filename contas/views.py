6daf2629690f9cb19d318513cdcd4416ec62cc6b
Adryan1928 <adryanoliveira1928@gmail.com>
2023-07-17 15:43:42 -0300

    ver_contas está concluído

--- contas/views.py
+++ contas/views.py
@@ -3,6 +3,8 @@ from perfil.models import Categoria
 from .models import ContaPaga, ContaPagar
 from django.contrib.messages import constants
 from django.contrib import messages
+from datetime import datetime
+from django.http import HttpResponse
 
 def definir_contas(request):
     if request.method == 'GET':
@@ -28,3 +30,37 @@ def definir_contas(request):
         messages.add_message(request, constants.SUCCESS, 'Conta cadastrada com sucesso')
 
         return redirect('/contas/definir_contas/')
+
+def ver_contas(request):
+    MES_ATUAL = datetime.now().month
+    DIA_ATUAL = datetime.now().day
+
+    contas = ContaPagar.objects.all()
+
+    contas_pagas = ContaPaga.objects.filter(data_pagamento__month= MES_ATUAL).values('conta')
+
+    contas_vencidas = contas.filter(dia_pagamento__lt = DIA_ATUAL).exclude(id__in = contas_pagas)
+
+    contas_proximas_vencimento = contas.filter(dia_pagamento__lte = DIA_ATUAL + 5).filter(dia_pagamento__gt = DIA_ATUAL).exclude(id__in = contas_pagas)
+
+    restantes = contas.exclude(id__in = contas_vencidas).exclude(id__in = contas_pagas).exclude(id__in = contas_proximas_vencimento)
+    
+    return render(request, 'ver_contas.html', {
+                    'contas_vencidas': contas_vencidas,
+                    'contas_proximas_vencimento': contas_proximas_vencimento,
+                    'restantes': restantes
+                })
+
+def pagar_conta(request, id):
+    data_pagamento = datetime.now()
+
+    conta = ContaPaga(
+        conta_id = id,
+        data_pagamento = data_pagamento
+    )
+
+    conta.save()
+
+    messages.add_message(request, constants.SUCCESS, 'Conta paga')
+
+    return redirect('/contas/ver_contas')
