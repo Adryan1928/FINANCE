514e799cc349543cc931e88a2fe72f5880ea3ccb
Adryan1928 <adryanoliveira1928@gmail.com>
2023-07-17 11:34:02 -0300

    definir_contas está completo

--- contas/views.py
+++ contas/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from perfil.models import Categoria
+from .models import ContaPaga, ContaPagar
+from django.contrib.messages import constants
+from django.contrib import messages
 
 def definir_contas(request):
     if request.method == 'GET':
         categorias = Categoria.objects.all()
         return render(request, 'definir_contas.html', {'categorias': categorias})
+    elif request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        categoria = request.POST.get('categoria')
+        descricao = request.POST.get('descricao')
+        valor = request.POST.get('valor')
+        dia_pagamento = request.POST.get('dia_pagamento')
+
+        conta = ContaPagar(
+            titulo = titulo,
+            categoria_id = categoria,
+            descricao = descricao,
+            valor = valor,
+            dia_pagamento = dia_pagamento
+        )
+
+        conta.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Conta cadastrada com sucesso')
+
+        return redirect('/contas/definir_contas/')
